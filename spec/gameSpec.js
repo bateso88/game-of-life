5643f993d0f6e063d16c797611e00a0c2d07498e
bateso88 <bateso@hotmail.co.uk>
2021-03-12 10:49:37 +0000

    add testing for edge cases

--- spec/gameSpec.js
+++ spec/gameSpec.js
@@ -25,23 +25,34 @@ describe("Game", function() {
       game = new Game(customStartingGrid)
       game.tick()
     });
-    it("live cells with no live neighbours die", function() {
+    it("live cells with 0 live neighbours die", function() {
       expect(game.startingGrid[4][4]).toEqual(0)
     });
-    it("live cells with one live neighbour die", function() {
+    it("live cells with 1 live neighbour die", function() {
       expect(game.startingGrid[2][1]).toEqual(0)
     });
-    it("live cells with two live neighbours stay alive", function() {
+    it("dead cells with less than 2 live neighbours stay dead", function() {
+      expect(game.startingGrid[0][4]).toEqual(0)
+    });
+    it("live cells with 2 live neighbours stay alive", function() {
       expect(game.startingGrid[2][2]).toEqual(1)
     });
-    it("live cells with three live neighbours stay alive", function() {
+    it("live cells with 3 live neighbours stay alive", function() {
       expect(game.startingGrid[3][2]).toEqual(1)
     });
-    it("dead cells with three live neighbours come alive", function() {
+    it("dead cells with 3 live neighbours come alive", function() {
       expect(game.startingGrid[2][3]).toEqual(1)
     });
-    it("dead cells with two live neighbours stay dead", function() {
+    it("dead cells with 2 live neighbours stay dead", function() {
       expect(game.startingGrid[1][1]).toEqual(0)
+    });
+    it("dead cells with more than 3 live neighbours stay dead", function() {
+      expect(game.startingGrid[1][2]).toEqual(0)
+    });
+    it("live cells with more than 3 live neighbours die", function() {
+      let game = new Game
+      game.tick()
+      expect(game.startingGrid[8][5]).toEqual(0)
     });
   });
 
